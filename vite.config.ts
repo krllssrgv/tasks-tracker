1b216d9aa56713eea5f0b0f8309c7637de4da8f2
kirill <krllssrgv@gmail.com>
2025-01-22 00:02:38 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'public',
+  },
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, './src/app/app'),
