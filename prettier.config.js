454e6d5070db68260cf88f3824a127caba0ed4b9
Kirill Savin <krllssrgv@gmail.com>
2025-01-21 13:59:46 +0300

    Добавил alias и prettier

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+export default {
+    trailingComma: 'es5',
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    printWidth: 80
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+    '@app': path.resolve(__dirname, './src/app/app'),
+    '@processes': path.resolve(__dirname, './src/processes/index'),
+    '@pages': path.resolve(__dirname, './src/pages/index'),
+    '@widgets': path.resolve(__dirname, './src/widgets/index'),
+    '@features': path.resolve(__dirname, './src/features/index'),
+    '@entities': path.resolve(__dirname, './src/entities/index'),
+    '@shared': path.resolve(__dirname, './src/shared/index'),
+    },
+  }
 })
